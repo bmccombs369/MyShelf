1d765b12fc58171fcebea7012ebfea27ebfe6a71
bmccombs369 <bmccombs369@gmail.com>
2018-06-08 10:49:23 -0400

    created index route for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  User
+  .find()
+  .then((users) => {
+    res.render('users/index', {
+      users: users
+    })
+  })
+  .catch((err) => res.send(err));
 });
 
 module.exports = router;
