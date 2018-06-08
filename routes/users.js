d3412fc404cdedf66ab0b7d83363203762661acb
bmccombs369 <bmccombs369@gmail.com>
2018-06-08 13:48:43 -0400

    created new and create routes

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,25 @@ const User = require('../models/User')
 /* GET users listing. */
 router.get('/', (req, res) => {
   User
-  .find({})
-  .then((users) => {
-    res.render('users/index', {
-      users: users
+    .find({})
+    .then((users) => {
+      res.render('users/index', {
+        users: users
+      })
     })
-  })
-  .catch((err) => res.send(err));
+    .catch((err) => res.send(err));
 });
+
+router.get('/new', (req, res) => {
+  res.render('users/new')
+});
+router.post('/', (req, res) => {
+  const newUser = req.body;
+  User.create(newUser)
+    .then(() => {
+      res.redirect('/users');
+    });
+});
+
 
 module.exports = router;
