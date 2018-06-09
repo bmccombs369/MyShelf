2ebe7ac74a3417e8095cea56c91f97bb753af9fa
bmccombs369 <bmccombs369@gmail.com>
2018-06-09 14:20:53 -0400

    added catalogsController to app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override');
 const indexRouter = require('./routes/index');
-const usersController = require('./routes/usersController');
-const catalogsController = require('./routes/catalogsController');
+const usersRouter = require('./routes/usersController');
+const catalogsController = require('./routes/catalogsController')
 
 const app = express();
 require('dotenv').config();
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersController);
+app.use('/users', usersRouter);
 app.use('/users/:userId/catalogs', catalogsController);
 
 // catch 404 and forward to error handler
