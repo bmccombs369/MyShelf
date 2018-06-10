7d1f99403e9a29700bfc9bb4a4bc3054f67036b0
bmccombs369 <bmccombs369@gmail.com>
2018-06-09 21:32:56 -0400

    created catalogs Index view

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersController);
-// \/This breaks server\/
 app.use('/users/:usersId/catalogs', catalogsController);
 
 // catch 404 and forward to error handler
