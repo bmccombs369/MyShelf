5f95d2886e8d1c5c98eb91026e5a21b5b45ccd17
bmccombs369 <bmccombs369@gmail.com>
2018-06-11 14:47:16 -0400

    edited catalogs index view

--- routes/catalogsController.js
+++ routes/catalogsController.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
         .then((user) => {
             const catalogs = user.catalogs
             res.render('catalogs/index', {
+                user: user,
                 catalogs: catalogs
             });
         });
