7f7058bb580268e0307328aca2831d765cb4987e
bmccombs369 <bmccombs369@gmail.com>
2018-06-12 13:02:48 -0400

    added button to add movies in catalogs show view

--- routes/moviesController.js
+++ routes/moviesController.js
@@ -3,17 +3,17 @@ const User = require('../models/User');
 const Catalog = require('../models/Catalog');
 const Movie = require('../models/Movie');
 
-router.get('/', (req, res) => {
+router.get('/new', (req, res) => {
   const usersId = req.params.usersId;
   const catalogsId = req.params.catalogsId;
   User.findById(usersId)
     .then((user) => {
-      const catalog = user.catalogs.id(catalogsId)
-      res.render('movies/index', {
+      const catalog = user.catalogs.id(catalogsId);
+      res.render('movies/new', {
         user: user,
         movies: movies
-      })
-    })
-})
+      });
+    });
+});
 
 module.exports = router;
